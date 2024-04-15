c1fac5f34642ab18713f4e3ea3e12f094cd5a48f
Therealirfanshafi <thatirfanshafi@gmail.com>
2024-04-15 19:44:50 +0600

    Completed the styles for the subject list

--- intial_template_code/script.js
+++ intial_template_code/script.js
@@ -1,13 +1,13 @@
 const subjectList = [];
 
 function addSubject(subjectName) {
-    document.querySelector('#selected-subject-list-container').style.display = "block";
+    document.querySelector('#selected-subject-list-container').style.display = "flex";
     document.querySelector('#submit').style.display = "block";
     const subjectListElm = document.getElementById('selected-subject-list');
     if (!(subjectList.includes(subjectName))) {
         subjectList.push(subjectName);
-        const listItemElm = document.createElement('li');
-        listItemElm.textContent = subjectName;
-        subjectListElm.appendChild(listItemElm);
+        const buttonElm = document.createElement('button');
+        buttonElm.textContent = subjectName;
+        subjectListElm.appendChild(buttonElm);
     }
 }
