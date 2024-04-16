3a69b8a7f0ebee7f3d070f3d7a99cb5e3e99560a
Therealirfanshafi <thatirfanshafi@gmail.com>
2024-04-16 13:47:56 +0600

    Added string methods to the model classes

--- main_code/routine_maker/main/models.py
+++ main_code/routine_maker/main/models.py
@@ -8,8 +8,16 @@ class Subject(models.Model):
     code = models.CharField(max_length=4, validators=[MinLengthValidator(4)])
 
 
+    def __str__(self):
+        return self.code
+
+
 class Level(models.Model):
-    Title = models.CharField(max_length=8)
+    title = models.CharField(max_length=8)
+
+
+    def __str__(self):
+        return self.title
 
 
 class Component(models.Model):
@@ -21,11 +29,19 @@ class Component(models.Model):
     options = models.ManyToManyField('Option', through='Option_Component')
 
 
+    def __str__(self):
+        return f"{self.subject.code}/{self.code}"
+
+
 class Option(models.Model):
     name = models.CharField(max_length=10)
     extension = models.CharField(max_length=15, null=True)
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     components = models.ManyToManyField('Component', through='Option_Component')
+
+    
+    def __str__(self):
+        return self.name
 
 class Option_Component(models.Model):
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
