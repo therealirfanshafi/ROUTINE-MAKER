068bc41fac325a4e6f94c79698bc40a997933036
Therealirfanshafi <thatirfanshafi@gmail.com>
2024-04-16 13:38:40 +0600

    Set up the database model

--- main_code/routine_maker/main/admin.py
+++ main_code/routine_maker/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here
+admin.site.register(Subject)
+admin.site.register(Level)
+admin.site.register(Component)
+admin.site.register(Option)
+admin.site.register(Option_Component)
+admin.site.register(Exam)
--- main_code/routine_maker/main/models.py
+++ main_code/routine_maker/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Subject(models.Model):
+    code = models.CharField(max_length=4, validators=[MinLengthValidator(4)])
+
+
+class Level(models.Model):
+    Title = models.CharField(max_length=8)
+
+
+class Component(models.Model):
+    code = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
+    title = models.CharField(max_length=20)
+    marks = models.IntegerField()
+    duration = models.CharField(max_length=20)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    options = models.ManyToManyField('Option', through='Option_Component')
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=10)
+    extension = models.CharField(max_length=15, null=True)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    components = models.ManyToManyField('Component', through='Option_Component')
+
+class Option_Component(models.Model):
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    component = models.ForeignKey(Component, on_delete= models.CASCADE)
+
+
+class Exam(models.Model):
+    date = models.DateField()
+    session = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
+    component = models.ForeignKey(Component, on_delete = models.CASCADE)
